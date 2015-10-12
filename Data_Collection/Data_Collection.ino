72c2868002b15034d114cff8d4519ad38931d449
Trent Lukaczyk <aerialhedgehog@gmail.com>
2015-10-11 17:49:35 -0700

    first hack at code cleanup

--- Data_Collection/Data_Collection.ino
+++ Data_Collection/Data_Collection.ino
@@ -1,57 +1,342 @@
 /** 
-SuaveDyno Data Collection
-Elias Wu and Trent Lukaczyk 2015
+SUAVE Dyno Data Collection
+Elias Wu and Trent Lukaczyk 
+October 2015
 
 Usage: 
-Plug in Arduino or upload sketch, bring up serial monitor at 38400 baud, wait 8 seconds, plug in ESC. Arm dyno, then press start test/emergency.
+Plug in Arduino or upload sketch, 
+bring up serial monitor at 38400 baud, wait 8 seconds. 
+Arm dyno, then press start test/emergency.
 At any point, long press start test/emergency to terminate and power down motor.
 
-In order to calibrate ESC, plug in Arduino or upload sketch, wait until start/emergency light flashes, plug in ESC, let ESC calibrate, then unplug and replug ESC.
+To calibrate ESC,
+hold the arming switch at startup,
+then power your ESC, when it's ready
+for the low PWM, push the arming switch again.
+
 **/
+
+// ------------------------------------------------------------------------------
+//   INCLUDES
+// ------------------------------------------------------------------------------
+
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
 #include <HX711.h>
 #include <Servo.h>
 
-//Constants for APM Power Module
+
+// ------------------------------------------------------------------------------
+//   GLOBALS
+// ------------------------------------------------------------------------------
+
+// High level settings
+#define PWM_THROTTLE_STEPS 10   // number of pwm steps
+#define TIME_DELAY_SETTLE  4000 // milliseconds to wait after each step
+#define N_SENSOR_SAMPLES   5    // number of samples per step
+
+// Scales
+#define SCALE_THRUST_CALIBRATION -419.66f
+#define SCALE_TORQUE_CALIBRATION -1256.36f
+#define SCALE_THRUST_CONSTANT    0.407747f
+#define SCALE_TORQUE_CONSTANT    0.1019f
+#define SCALE_THRUST_CHANNEL     128
+#define SCALE_TORQUE_CHANNEL     32
+HX711 scale_thrust(A2,A3);
+HX711 scale_torque(A2,A3);
+
+// ADC for Battery Voltage and Current
 #define VOLTAGE_CONSTANT 0.00190263
 #define CURRENT_CONSTANT 0.003237
-// HX711.DOUT	- pin #A2
-// HX711.PD_SCK	- pin #A3
+Adafruit_ADS1115 adc_battery;
 
-HX711 scale(A2,A3);
-HX711 scale2(A2,A3);
-Adafruit_ADS1115 ads;
+// Servo driver for throttle
+#define PWM_THROTTLE_LOW  1000
+#define PWM_THROTTLE_HIGH 2000
+Servo servo_throttle;
 
-Servo myservo;
+// RPM sensor
+#define RPM_NUMBER_CHANGES 5
+#define RPM_TIMEOUT        1000000 // [us]
+#define RPM_HIGH_THRESHOLD 300
 
-//ADC Values
-unsigned int ADCValue;
-double Voltage;
-double Vcc;
+// State Flags
+bool test_started = false;
+bool armed        = false;
 
-//Arducopter Power Module
-double VRaw; 
-double IRaw;
-float VFinal; 
-float IFinal;
-int16_t adc0, adc1, adc2, adc3;
+// Pins definitions
+int pin_startstop        = 6;
+int pin_arm              = 3;
+int pin_startstop_status = 4;
+int pin_arm_status       = 7;
+int pin_throttle         = 9;
+int pin_rpm              = A1;
 
-//Status LED
-int startEmergencyLed = 4;
-int armLed = 7;
 
-//booleans
-int testStarted = 0;
-int armed = 0;
+// ------------------------------------------------------------------------------
+//   SETUP
+// ------------------------------------------------------------------------------
 
-//Pins definitions
-int startEmergencyPin = 6;
-int armPin = 3;
-int rpmPin = 2;
+void setup() {
+    
+  // Run serial interface
+  Serial.begin(38400); 
+  
+  // Startup
+  Serial.println("SUAVE Motor Dyno");
+  Serial.print("Initializing ... ");
+  
+  // Set analog pin reference voltage
+  analogReference(DEFAULT);
+  
+  // Initialize PWM driver
+  servo_throttle.attach(pin_throttle);
+  servo_throttle.writeMicroseconds(PWM_THROTTLE_LOW-50);
+  
+  // Initialize Pins
+  pinMode(pin_startstop_status,OUTPUT);
+  pinMode(pin_startstop,INPUT_PULLUP);
+  pinMode(pin_arm,INPUT_PULLUP);
+  pinMode(pin_arm_status,OUTPUT);
+  adc_battery.begin();
+  
+  // Scale Initialization
+  tare_scales();
+  
+  // Startup complete
+  digitalWrite(pin_startstop_status,1); 
+  delay(500);
+  digitalWrite(pin_startstop_status,0);
+  Serial.println("Done!");
+  Serial.println("");
+  
+  // Check for ESC Calibration
+  if ( digitalRead(pin_arm) ) 
+    calibrate_esc();
+  
+  // Here we go!
+  Serial.println("Ready to Sample.");
+}
 
-//For calibrating the ADC
-long readVcc() {
+
+// ------------------------------------------------------------------------------
+//   LOOP
+// ------------------------------------------------------------------------------
+
+void loop() {
+  
+  // Check if device is being armed
+  if( digitalRead(pin_arm) == 0 ){ 
+    if( !armed ){
+      armed = true;
+      servo_throttle.writeMicroseconds(PWM_THROTTLE_LOW);
+      analogWrite(pin_arm_status,200);
+    }
+  }
+  
+  // Check if start button is pressed
+  if( armed && digitalRead(pin_startstop) == 0 ){ 
+    if( !test_started ){
+      test_started = true;
+      analogWrite(pin_startstop_status,200);
+      delay(2000);
+    }
+  }
+  
+  
+  // --------------------------------------------------------------------------
+  //   Run The Test
+  // --------------------------------------------------------------------------
+  
+  if( test_started ){
+    
+    // Tare the scales for each new test
+    tare_scales();
+    
+    // Run throttle setpoints
+    for(int i_thr = 0; i_thr < (PWM_THROTTLE_STEPS+1); i_thr++){
+      
+      // Set this throttle setpoint
+      int this_pwm_thrust = PWM_THROTTLE_LOW 
+                          + ( i_thr * (PWM_THROTTLE_HIGH-PWM_THROTTLE_LOW)
+                                    / PWM_THROTTLE_STEPS );
+      servo_throttle.writeMicroseconds(this_pwm_thrust);
+      
+      // Allow time to settle
+      delay(TIME_DELAY_SETTLE);
+      
+      // Run sensor readings
+      for(int i_samp = 0; i_samp < N_SENSOR_SAMPLES; i_samp++){
+        
+        // Throttle
+        Serial.print("Throttle: ");
+        Serial.print( 100.0f * ((float)i_thr)/((float)PWM_THROTTLE_STEPS) );
+        Serial.print(" %, ");
+        Serial.print("PWM: ");
+        Serial.print(this_pwm_thrust);
+        Serial.print(" us, ");
+
+        // Thrust
+        Serial.print("Thrust: ");
+        scale_thrust.set_gain(SCALE_THRUST_CHANNEL);
+        Serial.print( scale_thrust.get_units(1) * SCALE_THRUST_CONSTANT );
+        Serial.print(" g, ");
+        
+        // Torque
+        Serial.print("Torque: ");
+        scale_torque.set_gain(SCALE_TORQUE_CHANNEL);
+        Serial.print( scale_torque.get_units(1) * SCALE_TORQUE_CONSTANT );
+        Serial.print(" g-m, ");
+        
+        // Battery Voltage and Current
+        Serial.print("Voltage: ");
+        Serial.print( adc_battery.readADC_SingleEnded(0) * VOLTAGE_CONSTANT );
+        Serial.print(" V, ");
+        Serial.print("Current: ");
+        Serial.print( adc_battery.readADC_SingleEnded(1) * CURRENT_CONSTANT );
+        Serial.print(" A, ");
+        
+        // RPM
+        Serial.print("RPM: ");
+        Serial.print( measure_rpm() );
+        
+        // Next line
+        Serial.println();
+        
+        // Flushhhh
+        Serial.flush();
+        
+        // Emergency button press
+        if( digitalRead(pin_startstop) == 0 || test_started == 0){
+          Serial.println("EMERGENCY STOP!");
+          test_started = 0;
+          break;
+        }
+      }
+      
+      // Break out on emergency button press
+      if ( !test_started ) {
+        break;
+      }
+      
+    }
+    
+    // Stop Motor
+    servo_throttle.writeMicroseconds(PWM_THROTTLE_LOW-50);
+    
+    // LED Signal Stop
+    for(int i = 0; i < 5; i ++){ 
+      digitalWrite(pin_startstop_status,1);
+      delay(50);
+      digitalWrite(pin_startstop_status,0);
+      delay(50);
+    }
+    analogWrite(pin_startstop_status,0);
+    Serial.println();
+    
+    // Message for successful test completion
+    if ( test_started ) {
+      Serial.println("Test completed.");
+      Serial.println();
+      test_started = 0;
+    }
+    
+    // Clean up and done
+    Serial.flush();
+    delay(3000);
+    
+  }
+}
+
+
+// Measure RPM
+unsigned measure_rpm(){
+
+  int changes  = 0;
+  unsigned rpm = 0;
+  bool timeOut = false;
+
+  // get current time
+  long currtime = micros();                 
+  
+  while(changes < RPM_NUMBER_CHANGES && !timeOut){
+    if((micros()-currtime)>RPM_TIMEOUT) 
+      timeOut = true;
+    delayMicroseconds(1);
+    
+    if(analogRead(pin_rpm) < RPM_HIGH_THRESHOLD){
+      
+      while(analogRead(pin_rpm) < RPM_HIGH_THRESHOLD && !timeOut){        
+        if((micros()-currtime)>RPM_TIMEOUT) 
+          timeOut = true;
+        delayMicroseconds(1);
+        
+      }
+      if(analogRead(pin_rpm) > RPM_HIGH_THRESHOLD){
+        if(changes == 0) 
+          currtime = micros();
+        changes ++;
+      }
+    }
+  }
+  if (changes == RPM_NUMBER_CHANGES){
+    rpm = (((changes-1)* 60 * 1000000)/(micros()-currtime));
+  }
+  return rpm;
+    
+}
+
+
+//ESC Calibration
+void calibrate_esc() {  
+
+  //Write ESC high for calibration
+  servo_throttle.writeMicroseconds(PWM_THROTTLE_HIGH); 
+  
+  //Notify user that calibration has begun
+  Serial.print("Calibrate ESC ... ");
+  for(int i = 0; i < 3; i ++){ 
+    digitalWrite(pin_startstop_status,1);
+    delay(100);
+    digitalWrite(pin_startstop_status,0);
+    delay(100);
+  }
+  
+  // Wait for user to release then press arm switch again
+  while( digitalRead(pin_arm) )
+    delay(10);
+  while( !digitalRead(pin_arm) )
+    delay(10);
+  
+  servo_throttle.writeMicroseconds(PWM_THROTTLE_LOW);
+  
+  //Notify user that calibration is done
+  Serial.println("Done!");
+  for(int i = 0; i < 3; i ++){ 
+    digitalWrite(pin_startstop_status,1);
+    delay(100);
+    digitalWrite(pin_startstop_status,0);
+    delay(100);
+  }
+  
+  Serial.println("");
+  
+}
+
+
+// Tare Scales
+void tare_scales() {
+  scale_thrust.set_gain (SCALE_THRUST_CHANNEL);
+  scale_thrust.set_scale(SCALE_THRUST_CALIBRATION);
+  scale_thrust.tare();	
+  scale_torque.set_gain (SCALE_THRUST_CHANNEL);
+  scale_torque.set_scale(SCALE_TORQUE_CHANNEL);
+  scale_torque.tare();
+}
+
+
+// ADC Calibration
+long read_vcc() {
   long result;
   // Read 1.1V reference against AVcc
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
@@ -62,204 +347,4 @@ long readVcc() {
   result |= ADCH<<8;
   result = 1125300L / result; // Back-calculate AVcc in mV
   return result;
-}
-
-void setup() {
-  Serial.begin(38400); //Run serial
-  Serial.println("HX711 Demo");
-  analogReference(DEFAULT);
-  Vcc = readVcc()/1000.0; //calibrate the ADC
-  
-  myservo.attach(9); //Initialize PWM on pin 9
-//  delay(3000);
-  myservo.writeMicroseconds(950);
-  //Initialize Pins
-  pinMode(startEmergencyLed,OUTPUT);
-  pinMode(startEmergencyPin,INPUT_PULLUP);
-  pinMode(armPin,INPUT_PULLUP);
-  pinMode(armLed,OUTPUT);
-  pinMode(rpmPin,INPUT);
-  ads.begin();
-  /**
-  //ESC Calibration
-  myservo.writeMicroseconds(2000); //Write ESC high for calibration
-  for(int i = 0; i < 3; i ++){ //Notify user that calibration has begun
-    digitalWrite(startEmergencyLed,1);
-    delay(100);
-    digitalWrite(startEmergencyLed,0);
-    delay(100);
-  }
-  delay(2000);
-  myservo.writeMicroseconds(950);
-  **/
-  
-  digitalWrite(startEmergencyLed,1); //Notify user that calibration has finished
-  delay(1000);
-  digitalWrite(startEmergencyLed,0);
-  delay(1500);
-  
-  //HX711 Initialization
-  scale.set_gain(128);
-  scale.set_scale(-409.08f); //This value needs to be calibrated.
-  scale.tare();	
-  scale2.set_gain(32);
-  scale2.set_scale(1042.36f); //This value needs to be calibrated
-  scale2.tare();
-
-  Serial.println("Readings:");
-}
-
-void loop() {
-  if(digitalRead(armPin) == 0){ //check if device is being armed
-    if(armed == 0){
-      armed = 1;
-      myservo.writeMicroseconds(950);
-      analogWrite(armLed,200);
-    }
-  }
-  if(digitalRead(startEmergencyPin) == 0&&armed == 1){ //check if start button is pressed
-    if(testStarted==0){
-      testStarted = 1;
-      analogWrite(startEmergencyLed,200);
-      delay(2000);
-    }
-  }
-  if(testStarted == 1){
-    //Scales need to be re-initialized every loop in order to read the different load cells.
-    scale.set_gain(128);
-    scale.set_scale(-419.66f);
-    scale.tare();	
-    scale2.set_gain(32);
-    scale2.set_scale(-1256.36f);
-    scale2.tare();
-    
-    //Run 10 throttle settings
-    for(int i = 0; i < 11; i++){
-      int thrustValue = 950+(i*100);
-      myservo.writeMicroseconds(thrustValue);
-      delay(4000);
-      
-      //Run 5 sensor readings
-      for(int j = 0; j < 5; j++){
-
-        
-        //Read voltage and current
-//        VRaw = (analogRead(vPin) / 1023.0) * Vcc;
-//        IRaw = (analogRead(iPin) / 1023.0) * Vcc;
-//        VFinal = VRaw*VOLTAGE_CONSTANT; 
-//        IFinal = (IRaw*CURRENT_CONSTANT)-.345; 
-//        if(VFinal < 0) VFinal = 0;
-//        if(IFinal < 0) IFinal = 0;
-
-        adc0 = ads.readADC_SingleEnded(0);
-        adc1 = ads.readADC_SingleEnded(1);
-  
-        //Set up to read thrust cell
-        scale.set_gain(128);
-        Serial.print("Thrust: ");
-        Serial.print(scale.get_units(1)/2.4525);
-        Serial.print("g,");
-        Serial.print(" Torque: ");
-        
-        //Set up to read torque cell
-        scale2.set_gain(32);
-        Serial.print(scale2.get_units(1)*0.1019);
-        Serial.print("g-m,");
-        
-        //Voltage and current
-        Serial.print(" Voltage: ");
-        Serial.print(adc0*VOLTAGE_CONSTANT);
-        Serial.print("V,");
-        Serial.print(" Current: ");
-        Serial.print(adc1*CURRENT_CONSTANT);
-        Serial.print("A,");
-        
-        //Control values
-        Serial.print(" PWM: ");
-        Serial.print(thrustValue);
-        Serial.print(",");
-        Serial.print(" Throttle: ");
-        Serial.print(((thrustValue-950)/1000.0)*100);
-        Serial.print("%,");
-        Serial.print(" RPM: ");
-        Serial.println(measureRPM());
-        Serial.flush();
-        
-        //Emergency button press
-        if(digitalRead(startEmergencyPin) == 0||testStarted == 0){
-          myservo.writeMicroseconds(950);
-          for(int i = 0; i < 5; i ++){ //Notify user that emergency stop triggered
-            digitalWrite(startEmergencyLed,1);
-            delay(50);
-            digitalWrite(startEmergencyLed,0);
-            delay(50);
-          }
-          testStarted = 0;
-          break;
-        }
-      }
-      if(digitalRead(startEmergencyPin) == 0||testStarted == 0){
-        myservo.writeMicroseconds(950);
-        for(int i = 0; i < 5; i ++){ //Notify user that emergency stop triggered
-          digitalWrite(startEmergencyLed,1);
-          delay(50);
-          digitalWrite(startEmergencyLed,0);
-          delay(50);
-        }
-        testStarted = 0;
-        Serial.println("EMERGENCY STOP!");
-        break;
-      }
-    }
-    myservo.writeMicroseconds(950);
-    testStarted = 0;
-    analogWrite(startEmergencyLed,0);
-    Serial.println();
-    Serial.println("Test completed.\n");
-    Serial.flush();
-    delay(3000);
-  }
-}
-
-#define RPM_NUMBER_CHANGES 5
-#define RPM_TIMEOUT 1000000 // [us]
-#define RPM_HIGH_THRESHOLD 300
-
-//Blocking measure RPM using EagleTree optical RPM sensor
-unsigned measureRPM(){
-  long currtime = micros();                 // GET CURRENT TIME
-  int changes = 0;
-  unsigned rpm = 0;
-  bool timeOut = false;
-  while(changes < RPM_NUMBER_CHANGES && !timeOut){
-    
-    if((micros()-currtime)>RPM_TIMEOUT) 
-      timeOut = true;
-    
-    delayMicroseconds(1);
-    
-    //if(digitalRead(2) == 0){
-    if(analogRead(A1) < RPM_HIGH_THRESHOLD){
-      
-      while(analogRead(A1) < RPM_HIGH_THRESHOLD && !timeOut){
-        
-        if((micros()-currtime)>RPM_TIMEOUT) 
-          timeOut = true;
-          
-        delayMicroseconds(1);
-        
-      }
-      if(analogRead(A1) > RPM_HIGH_THRESHOLD){
-        if(changes == 0) 
-          currtime = micros();
-          
-        changes ++;
-      }
-    }
-  }
-  if (changes == RPM_NUMBER_CHANGES){
-    rpm = (((changes-1)* 60 * 1000000)/(micros()-currtime)) ;       //  CALCULATE  RPM USING REVOLUTIONS AND ELAPSED TIM
-  }
-  return rpm;
-    
 }
